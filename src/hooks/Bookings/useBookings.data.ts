fca5217631a6824b9ad20fbd9d48c1439a2b93c3
luigiProactive <luigi@arproactive.com>
2023-12-08 16:22:15 -0300

    Update url images

--- src/hooks/Bookings/useBookings.data.ts
+++ src/hooks/Bookings/useBookings.data.ts
@@ -4,7 +4,7 @@ export const BOOKINGS: Booking[] = [
   {
     id: 1,
     amount: 800,
-    imageUrl: './src/assets/img/cards/paris.jpeg',
+    imageUrl: '/assets/img/cards/paris.jpeg',
     title: 'London',
     month: 'January',
     year: 2024
@@ -12,7 +12,7 @@ export const BOOKINGS: Booking[] = [
   {
     id: 2,
     amount: 850,
-    imageUrl: './src/assets/img/cards/paris.jpeg',
+    imageUrl: '/assets/img/cards/paris.jpeg',
     title: 'London',
     month: 'February',
     year: 2024
